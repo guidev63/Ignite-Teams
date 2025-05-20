1f5e26494970f409dde5fa41bbbda7e23888636a
Guilherme Augusto <guilhermedev23@gmail.com>
2025-05-20 15:27:50 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
  import { StatusBar } from 'react-native';
 
-import { Groups } from '@screens/Groups/index';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { Loading } from '@components/Loading';
+import { NewGroup } from '@screens/NewGroup';
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
@@ -18,7 +18,7 @@ export default function App() {
        backgroundColor='transparent'
        translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }

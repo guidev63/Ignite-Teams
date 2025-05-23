a8e9667a78ca837cfaf7ff227a08f00e44a1530b
guidev63 <guilhermedev23@gmail.com>
2025-05-22 22:24:46 -0300

    recriando repositório

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,25 +5,28 @@ import { Button } from '@components/Button';
 import { Input } from '@components/input/input';
 
 export function NewGroup() {
-    console.log("Alteração real no código");
-git add .
-git commit -m "recriando repositório"
+  console.log("Teste de commit");
+  return (
+    <Container>
+      <Header showBackButton />
 
-    return (
-        <Container>
-            <Header showBackButton />
-            <Content>
-                <Icon />
-                <Highlight 
-                    title="Nova Turma"
-                    subtitle="Crie a turma para Adicionar as Pessoas"
-                />
-                < Input/>
-                <Button 
-                    title="Criar"
-                    style={{ marginTop: 20}}
-                />
-            </Content>
-        </Container>
-    );
+      <Content>
+        <Icon />
+
+        <Highlight
+          title="Nova Turma"
+          subtitle="Crie a turma para Adicionar as Pessoas"
+        />
+
+        <Input
+          placeholder="Nome da Turma"
+        />
+
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+        />
+      </Content>
+    </Container>
+  );
 }

544c73138d870b2896faa5655d95789bc982e7ce
guidev63 <guilhermedev23@gmail.com>
2025-05-29 17:06:40 -0300

    ok

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,9 +3,14 @@ import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/input/input';
+import { useNavigation } from '@react-navigation/native';
 
 export function NewGroup() {
-  console.log("Teste de commit");
+  const navigation = useNavigation();
+ function handleNew(){
+navigation.navigate('players' , { group:'Rocket'});
+ }
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,6 +30,7 @@ export function NewGroup() {
         <Button
           title="Criar"
           style={{ marginTop: 20 }}
+          onPress={handleNew}
         />
       </Content>
     </Container>
